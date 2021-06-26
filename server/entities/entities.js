99a54f65541e36d3a2a67ca311a962fe6b0021b8
decadence <decadence127@gmail.com>
2021-06-26 19:05:53 +0300

    created entities

---
+++ server/entities/entities.js
@@ -0,0 +1,92 @@
+const sequelize = require('../database')
+
+const {DataTypes} = require('sequelize')
+
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email:{type: DataTypes.STRING, unique: true},
+    password:{type: DataTypes.STRING},
+    role:{type: DataTypes.STRING, defaultValue: "USER"},
+
+})
+
+const Cart = sequelize.define('cart',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const DeviceCart = sequelize.define('device-cart',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Device = sequelize.define('device',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull:false},
+    img: {type: DataTypes.STRING, allowNull:false},
+    rating: {type: DataTypes.INTEGER, defaultValue: 0},
+
+})
+
+const Type = sequelize.define('type',{
+    id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type:DataTypes.STRING, unique: true, allowNull: false}
+})
+const Brand = sequelize.define('brand',{
+    id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type:DataTypes.STRING, unique: true, allowNull: false}
+})
+
+const Rating = sequelize.define('rating',{
+    id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type:DataTypes.INTEGER, allowNull: false}
+})
+
+const DeviceDesc = sequelize.define('device-desc',{
+    id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type:DataTypes.STRING, allowNull: false},
+    description: {type:DataTypes.STRING, allowNull: false}
+})
+
+User.hasOne(Cart)
+User.hasMany(Rating)
+Rating.belongsTo(User)
+Cart.belongsTo(User)
+
+Cart.hasMany(DeviceCart)
+DeviceCart.belongsTo(Cart)
+
+
+Type.hasMany(Device)
+Device.belongsTo(Type)
+
+Brand.hasMany(Device)
+Device.belongsTo(Brand)
+
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
+
+Device.hasMany(DeviceCart)
+DeviceCart.belongsTo(Device)
+
+Device.hasMany(DeviceDesc)
+DeviceDesc.belongsTo(Device)
+
+const TypeBrand = sequelize.define('type-brand', {
+    id:{type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true}
+})
+
+Type.belongsToMany(Brand, {through: TypeBrand})
+Brand.belongsToMany(Type,  {through: TypeBrand})
+
+module.exports = {
+    User, 
+    Cart, 
+    DeviceCart, 
+    Device,
+    Type, 
+    Brand,
+    DeviceDesc,
+    TypeBrand,
+    Rating
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 require('dotenv').config()
+
+const entities = require('./entities/entities')
 const sequelize = require('./database')
 const PORT = process.env.PORT || 5050
-
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'OK'})
+})
 
 
 const start = async () =>{
