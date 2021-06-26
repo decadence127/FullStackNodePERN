25fa3fcb1adfb956138b632a6b55c6c3fa0e0fe0
decadence <decadence127@gmail.com>
2021-06-26 19:28:24 +0300

    added controller's bodies

---
+++ server/controllers/userController.js
@@ -0,0 +1,16 @@
+class UserController {
+    async registration (req, res)
+    {
+
+    }
+    async login(req, res)
+    {
+
+    }
+    async validate(req,res)
+    {
+        
+    }
+}
+
+module.exports = new UserController()
